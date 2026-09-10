599e0cfc1028ef422ab4534fb6989a720853af2d
priti <priti12bishnoi@gmail.com>
2026-09-10 09:44:36 +0530

    intial

---
+++ README.md
@@ -0,0 +1,57 @@
+
+**Prerequisites**
+
+Make sure you have the following installed on your machine:
+
+
+- [Node.js](https://nodejs.org/en)
+- [npm](https://www.npmjs.com/) (Node Package Manager)
+
+**Cloning the Repository**
+
+```bash
+git clone https://github.com/speakHire.git
+cd speakHire
+```
+
+**Installation**
+
+Install the project dependencies using npm:
+
+```bash
+npm install
+```
+
+**Set Up Environment Variables**
+
+Create a new file named `.env.local` in the root of your project and add the following content:
+
+```env
+NEXT_PUBLIC_VAPI_WEB_TOKEN=
+NEXT_PUBLIC_VAPI_WORKFLOW_ID=
+
+GOOGLE_GENERATIVE_AI_API_KEY=
+
+NEXT_PUBLIC_BASE_URL=
+
+NEXT_PUBLIC_FIREBASE_API_KEY=
+NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN=
+NEXT_PUBLIC_FIREBASE_PROJECT_ID=
+NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET=
+NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID=
+NEXT_PUBLIC_FIREBASE_APP_ID=
+
+FIREBASE_PROJECT_ID=
+FIREBASE_CLIENT_EMAIL=
+FIREBASE_PRIVATE_KEY=
+```
+
+Replace the placeholder values with your actual **[Firebase](https://firebase.google.com/)**, **[Vapi](https://vapi.ai/?utm_source=youtube&utm_medium=video&utm_campaign=jsmastery_recruitingpractice&utm_content=paid_partner&utm_term=recruitingpractice)** credentials.
+
+**Running the Project**
+
+```bash
+npm run dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) in your browser to view the project.
---
+++ app/api/vapi/generate/route.ts
@@ -0,0 +1,82 @@
+import { generateObject } from "ai";
+import { z } from "zod";
+import { google } from "@ai-sdk/google";
+
+import { db } from "@/firebase/admin";
+import { getRandomInterviewCover } from "@/lib/utils";
+
+export async function POST(request: Request) {
+  const body = await request.json();
+  console.log("Vapi Payload:", JSON.stringify(body, null, 2));
+  
+  let params = body;
+  let toolCallId;
+  
+  if (body.message?.toolWithToolCallList) {
+    const toolCall = body.message.toolWithToolCallList[0].toolCall;
+    params = toolCall.function.arguments;
+    toolCallId = toolCall.id;
+  } else if (body.message?.toolCalls) {
+    params = body.message.toolCalls[0].function.arguments;
+    toolCallId = body.message.toolCalls[0].id;
+  }
+  
+  let { type, role, level, techstack, amount, userid } = params;
+
+  if (!userid && body.message?.call?.variableValues?.userid) {
+    userid = body.message.call.variableValues.userid;
+  }
+  
+  if (!techstack) techstack = "General";
+  try {
+    const { object: parsedQuestions } = await generateObject({
+      model: google("gemini-3.6-flash"),
+      schema: z.array(z.string()),
+      prompt: `Prepare ${amount || 5} questions for a job interview.
+        The job role is ${role}.
+        The job experience level is ${level}.
+        The tech stack used in the job is: ${techstack}.
+        The focus between behavioural and technical questions should lean towards: ${type}.
+        Please return only the questions, without any additional text.
+        The questions are going to be read by a voice assistant so do not use "/" or "*" or any other special characters which might break the voice assistant.
+    `,
+    });
+
+    const interview = {
+      role: role,
+      type: type || "Mixed",
+      level: level,
+      techstack: techstack.split(","),
+      questions: parsedQuestions,
+      userId: userid,
+      finalized: true,
+      coverImage: getRandomInterviewCover(),
+      createdAt: new Date().toISOString(),
+    };
+
+    await db.collection("interviews").add(interview);
+
+    if (toolCallId) {
+      return Response.json(
+        {
+          results: [
+            {
+              toolCallId,
+              result: "Interview generated and saved successfully.",
+            },
+          ],
+        },
+        { status: 200 }
+      );
+    }
+
+    return Response.json({ success: true }, { status: 200 });
+  } catch (error) {
+    console.error("Error:", error);
+    return Response.json({ success: false, error: error }, { status: 500 });
+  }
+}
+
+export async function GET() {
+  return Response.json({ success: true, data: "Thank you!" }, { status: 200 });
+}
